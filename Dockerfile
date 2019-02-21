24273a87027eb15018be8dc9d348879be2b99a66
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2019-02-21 14:31:08 -0600

    update version number

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:cosmic-20190131
 
 MAINTAINER Jeremiah H. Savage <jeremiahsavage@gmail.com>
 
-ENV version 0.1
+ENV version 0.2
 
 RUN apt-get update \
     && export DEBIAN_FRONTEND=noninteractive \
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name = 'bam_readgroup_to_gdc_json',
       author = 'Jeremiah H. Savage',
       author_email = 'jeremiahsavage@gmail.com',
-      version = 0.21,
+      version = 0.2,
       description = 'convert each readgroup id to a json file from a BAM',
       url = 'https://github.com/NCI-GDC/bam_readgroup_to_gdc_json/',
       license = 'Apache 2.0',
