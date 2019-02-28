cf5331744acafc21a382c32c7808e3178fe81ec9
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2019-02-28 17:29:07 -0600

    perform list operations and file operations in separate functions

--- bam_readgroup_to_gdc_json/extract_readgroup.py
+++ bam_readgroup_to_gdc_json/extract_readgroup.py
@@ -153,83 +153,76 @@ def harmonize_readgroup(readgroup_dict, logger):
         readgroup_dict['DT'] = dt
     return readgroup_dict
 
-def get_readgroup_dict_list(samfile, logger):
-    samfile_header = samfile.header
-    bam_readgroup_dict_list = samfile_header.get('RG')
+def get_readgroup_dict_list(bam_readgroup_dict_list, logger):
+    out_readgroup_dict_list = list()
+    for bam_readgroup_dict in bam_readgroup_dict_list:
+        # logger.debug('bam_readgroup_dict: {}'.format(bam_readgroup_dict))
+        rg = harmonize_readgroup(bam_readgroup_dict, logger)
+        readgroup_meta = dict()
+        readgroup_meta['aliquots'] = dict()
+        readgroup_meta['aliquots']['submitter_id'] = rg.get('SM', 'REQUIRED<string>')
+        readgroup_meta['experiment_name'] = rg.get('DS', 'REQUIRED<string>')
+        readgroup_meta['library_name'] = rg.get('LB', 'REQUIRED<string>')
+        readgroup_meta['platform'] = rg.get('PL', 'REQUIRED<enumeration>')
+        readgroup_meta['read_group_name'] = rg.get('ID', 'REQUIRED<string>')
+        readgroup_meta['sequencing_center'] = rg.get('CN', 'REQUIRED<string>')
+        if readgroup_meta['aliquots']['submitter_id'] != 'REQUIRED<string>':
+            readgroup_meta['submitter_id'] = readgroup_meta['aliquots']['submitter_id']+'_'+readgroup_meta['read_group_name']
+        else:
+            readgroup_meta['submitter_id'] = 'REQUIRED<string>'
+        readgroup_meta['is_paired_end'] = 'REQUIRED<boolean>'
+        readgroup_meta['library_selection'] = 'REQUIRED<enumeration>'
+        readgroup_meta['library_strategy'] = 'REQUIRED<enumeration>'
+        readgroup_meta['read_length'] = 'REQUIRED<integer,null>'
+        readgroup_meta['target_capture_kit'] = 'REQUIRED<enumeration>'
 
-    if not bam_readgroup_dict_list:
-        logger.error('There are no readgroups in BAM: {}'.format(samfile.filename))
-        raise NoReadGroupError
-    else:
-        out_readgroup_dict_list = list()
-        for bam_readgroup_dict in bam_readgroup_dict_list:
-            # logger.debug('bam_readgroup_dict: {}'.format(bam_readgroup_dict))
-            rg = harmonize_readgroup(bam_readgroup_dict, logger)
-            readgroup_meta = dict()
-            readgroup_meta['aliquots'] = dict()
-            readgroup_meta['aliquots']['submitter_id'] = rg.get('SM', 'REQUIRED<string>')
-            readgroup_meta['experiment_name'] = rg.get('DS', 'REQUIRED<string>')
-            readgroup_meta['library_name'] = rg.get('LB', 'REQUIRED<string>')
-            readgroup_meta['platform'] = rg.get('PL', 'REQUIRED<enumeration>')
-            readgroup_meta['read_group_name'] = rg.get('ID', 'REQUIRED<string>')
-            readgroup_meta['sequencing_center'] = rg.get('CN', 'REQUIRED<string>')
-            if readgroup_meta['aliquots']['submitter_id'] != 'REQUIRED<string>':
-                readgroup_meta['submitter_id'] = readgroup_meta['aliquots']['submitter_id']+'_'+readgroup_meta['read_group_name']
-            else:
-                readgroup_meta['submitter_id'] = 'REQUIRED<string>'
-            readgroup_meta['is_paired_end'] = 'REQUIRED<boolean>'
-            readgroup_meta['library_selection'] = 'REQUIRED<enumeration>'
-            readgroup_meta['library_strategy'] = 'REQUIRED<enumeration>'
-            readgroup_meta['read_length'] = 'REQUIRED<integer,null>'
-            readgroup_meta['target_capture_kit'] = 'REQUIRED<enumeration>'
+        # possible use
+        barcode_sequence = rg.get('BC', None) # CHECK_MATCH with multiplex_barcode, if present, below
+        description = rg.get('DS', None) # experiment?
 
-            # possible use
-            barcode_sequence = rg.get('BC', None) # CHECK_MATCH with multiplex_barcode, if present, below
-            description = rg.get('DS', None) # experiment?
+        predicted_median_insert_size = rg.get('PI', None)
+        readgroup_meta['instrument_model'] = rg.get('PM', 'OPTIONAL<enumeration>')
+        readgroup_meta['sequencing_date'] = rg.get('DT', 'OPTIONAL<ISO8601 date or date/time, null>')
 
-            predicted_median_insert_size = rg.get('PI', None)
-            readgroup_meta['instrument_model'] = rg.get('PM', 'OPTIONAL<enumeration>')
-            readgroup_meta['sequencing_date'] = rg.get('DT', 'OPTIONAL<ISO8601 date or date/time, null>')
+        platform_unit = rg.get('PU', None)
+        pu_dict = resolve_platform_unit(platform_unit)
+        if pu_dict:
+            readgroup_meta['flow_cell_barcode'] = pu_dict.get('FB', 'OPTIONAL<string>')
+            readgroup_meta['lane_number'] = pu_dict.get('LN', 'OPTIONAL<integer>')
+            readgroup_meta['multiplex_barcode'] = pu_dict.get('MB', 'OPTIONAL<string>')
 
-            platform_unit = rg.get('PU', None)
-            pu_dict = resolve_platform_unit(platform_unit)
-            if pu_dict:
-                readgroup_meta['flow_cell_barcode'] = pu_dict.get('FB', 'OPTIONAL<string>')
-                readgroup_meta['lane_number'] = pu_dict.get('LN', 'OPTIONAL<integer>')
-                readgroup_meta['multiplex_barcode'] = pu_dict.get('MB', 'OPTIONAL<string>')
+            #CHECK_MATCH
+            if barcode_sequence and readgroup_meta['multiplex_barcode'] != 'OPTIONAL<string>':
+                if barcode_sequence != readgroup_meta['multiplex_barcode']:
+                    logger.info('In Read Group {0}, the BC tag ({1}) does not match the third dotted part of the PU tag ({2})'.format(
+                        readgroup_meta['read_group_name'], barcode_sequence, readgroup_meta['multiplex_barcode']))
+        else:
+            readgroup_meta['flow_cell_barcode'] = 'OPTIONAL<string>'
+            readgroup_meta['lane_number'] = 'OPTIONAL<integer>'
+            readgroup_meta['multiplex_barcode'] = 'OPTIONAL<string>'
 
-                #CHECK_MATCH
-                if barcode_sequence and readgroup_meta['multiplex_barcode'] != 'OPTIONAL<string>':
-                    if barcode_sequence != readgroup_meta['multiplex_barcode']:
-                        logger.info('In Read Group {0}, the BC tag ({1}) does not match the third dotted part of the PU tag ({2})'.format(
-                            readgroup_meta['read_group_name'], barcode_sequence, readgroup_meta['multiplex_barcode']))
-            else:
-                readgroup_meta['flow_cell_barcode'] = 'OPTIONAL<string>'
-                readgroup_meta['lane_number'] = 'OPTIONAL<integer>'
-                readgroup_meta['multiplex_barcode'] = 'OPTIONAL<string>'
-
-            readgroup_meta['RIN'] = 'OPTIONAL<number>'
-            readgroup_meta['adapter_name'] = 'OPTIONAL<string>'
-            readgroup_meta['adapter_sequence'] = 'OPTIONAL<string>'
-            readgroup_meta['base_caller_name'] = 'OPTIONAL<string>'
-            readgroup_meta['base_caller_version'] = 'OPTIONAL<string>'
-            readgroup_meta['days_to_sequencing'] = 'OPTIONAL<integer>'
-            readgroup_meta['fragment_maximum_length'] = 'OPTIONAL<integer>'
-            readgroup_meta['fragment_mean_length'] = 'OPTIONAL<number>'
-            readgroup_meta['fragment_minimum_length'] = 'OPTIONAL<integer>'
-            readgroup_meta['fragment_standard_deviation_length'] = 'OPTIONAL<number>'
-            readgroup_meta['includes_spike_ins'] = 'OPTIONAL<boolean>'
-            readgroup_meta['library_preparation_kit_catalog_number'] = 'OPTIONAL<string>'
-            readgroup_meta['library_preparation_kit_name'] = 'OPTIONAL<string>'
-            readgroup_meta['library_preparation_kit_vendor'] = 'OPTIONAL<string>'
-            readgroup_meta['library_preparation_kit_version'] = 'OPTIONAL<string>'
-            readgroup_meta['library_strand'] = 'OPTIONAL<enumeration>'
-            readgroup_meta['size_selection_range'] = 'OPTIONAL<string>'
-            readgroup_meta['spike_ins_concentration'] = 'OPTIONAL<string>'
-            readgroup_meta['spike_ins_fasta'] = 'OPTIONAL<string>'
-            readgroup_meta['to_trim_adapter_sequence'] = 'OPTIONAL<boolean>'
-            readgroup_meta['type'] = 'read_group'
-            out_readgroup_dict_list.append(readgroup_meta)
+        readgroup_meta['RIN'] = 'OPTIONAL<number>'
+        readgroup_meta['adapter_name'] = 'OPTIONAL<string>'
+        readgroup_meta['adapter_sequence'] = 'OPTIONAL<string>'
+        readgroup_meta['base_caller_name'] = 'OPTIONAL<string>'
+        readgroup_meta['base_caller_version'] = 'OPTIONAL<string>'
+        readgroup_meta['days_to_sequencing'] = 'OPTIONAL<integer>'
+        readgroup_meta['fragment_maximum_length'] = 'OPTIONAL<integer>'
+        readgroup_meta['fragment_mean_length'] = 'OPTIONAL<number>'
+        readgroup_meta['fragment_minimum_length'] = 'OPTIONAL<integer>'
+        readgroup_meta['fragment_standard_deviation_length'] = 'OPTIONAL<number>'
+        readgroup_meta['includes_spike_ins'] = 'OPTIONAL<boolean>'
+        readgroup_meta['library_preparation_kit_catalog_number'] = 'OPTIONAL<string>'
+        readgroup_meta['library_preparation_kit_name'] = 'OPTIONAL<string>'
+        readgroup_meta['library_preparation_kit_vendor'] = 'OPTIONAL<string>'
+        readgroup_meta['library_preparation_kit_version'] = 'OPTIONAL<string>'
+        readgroup_meta['library_strand'] = 'OPTIONAL<enumeration>'
+        readgroup_meta['size_selection_range'] = 'OPTIONAL<string>'
+        readgroup_meta['spike_ins_concentration'] = 'OPTIONAL<string>'
+        readgroup_meta['spike_ins_fasta'] = 'OPTIONAL<string>'
+        readgroup_meta['to_trim_adapter_sequence'] = 'OPTIONAL<boolean>'
+        readgroup_meta['type'] = 'read_group'
+        out_readgroup_dict_list.append(readgroup_meta)
     return out_readgroup_dict_list
 
 def extract_readgroup_json(bam_path, logger):
@@ -241,6 +234,11 @@ def extract_readgroup_json(bam_path, logger):
         if not samfile.is_bam:
             logger.error("This program only runs on BAM files.")
             raise NotABamError
+        samfile_header = samfile.header
+        bam_readgroup_dict_list = samfile_header.get('RG')
+        if not bam_readgroup_dict_list:
+            logger.error('There are no readgroups in BAM: {}'.format(samfile.filename))
+            raise NoReadGroupError
         readgroup_dict_list = get_readgroup_dict_list(samfile, logger)
     with open(readgroups_json_file, 'w') as f:
         json.dump(readgroup_dict_list, f, indent=4)
