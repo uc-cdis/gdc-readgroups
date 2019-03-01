572f5fa1dae9144fb6f606705d6caf7c88172eac
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2019-03-01 11:25:07 -0600

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name = 'bam_readgroup_to_gdc_json',
       author = 'Jeremiah H. Savage',
       author_email = 'jeremiahsavage@gmail.com',
-      version = 0.12,
+      version = 0.13,
       description = 'From a BAM, convert each readgroup to a json object.',
       url = 'https://github.com/NCI-GDC/bam_readgroup_to_gdc_json/',
       license = 'Apache 2.0',
