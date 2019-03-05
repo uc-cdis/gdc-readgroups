46db56a427540ebdb4e74fa52d3e651f05b94297
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2019-03-05 00:55:20 -0600

    file str at top

--- bam_readgroup_to_gdc_json/generate_template.py
+++ bam_readgroup_to_gdc_json/generate_template.py
@@ -2,8 +2,9 @@ import json
 import os
 
 def get_readgroup_template():
+    json_file = 'readgroup_template.json'
     cwd = os.path.dirname(os.path.realpath(__file__))
-    json_path = os.path.join(cwd, 'readgroup_template.json')
+    json_path = os.path.join(cwd, json_file)
     with open(json_path, 'r') as f_open:
         data = json.load(f_open)
     return data
